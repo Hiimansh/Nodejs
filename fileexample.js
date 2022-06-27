b96a8f0b6a8cec022ec8f07d362b31c7e83345e2
Hiimansh <85495941+Hiimansh@users.noreply.github.com>
2022-06-27 09:49:34 +0530

    Add files via upload

--- fileexample.js
+++ fileexample.js
@@ -6,14 +6,46 @@
 const fs= require('fs')
 
 //readfile
-fs.readFile('./abc.txt', (error, data)=>{
-    if(error){
-        console.log("error");
-        console.log(error);
-    }
-    else{
-        console.log(data.toString());
-    }
-});
+// fs.readFile('./abc.txt', (error, data)=>{
+//     if(error){
+//         console.log("error");
+//         console.log(error);
+//     }
+//     else{
+//         console.log(data.toString());
+//     }
+// });
 
-console.log("terminated")
+// console.log("terminated")
+
+//write
+
+// let content= "this is a example of synamic content for write operation in nodejs.";
+// fs.writeFile('new_file.txt', content, (err)=>{
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("saved");
+//     }
+// })
+
+//append
+
+// fs.appendFile("./new_file.txt", "new content", (err)=>{
+//     if(err)
+//     {
+//         console.log("error"+ err);
+//     }else{
+//         console.log("saved");
+//     }
+// })
+
+//deletefile
+
+// fs.unlink("./fs.txt", (err)=>{
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("deleted");
+//     }
+// })
